e239400c55d36f429b8419b2f35894d6a1e9c610
Eirik Mo <eirik@mowebdev.com>
2023-12-19 18:15:41 +0100

    feat: add modal with context

--- App.tsx
+++ App.tsx
@@ -1,14 +1,23 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import { useState } from "react";
 import {
+    Dispatch,
+    SetStateAction,
+    createContext,
+    useContext,
+    useState,
+} from "react";
+import {
+    Alert,
     FlatList,
     Image,
+    Modal,
+    Pressable,
     SafeAreaView,
     StyleSheet,
     Text,
     View,
 } from "react-native";
-import { useAlbumsQuery, type AlbumsPage } from "./types/graphql";
+import { Album, useAlbumsQuery, type AlbumsPage } from "./types/graphql";
 
 const LIMIT = 10;
 const DEFAULT_PAGE = 1;
@@ -33,16 +42,29 @@ const client = new ApolloClient({
     },
   }),
 });
+const ModalContext = createContext<{
+  setModalData: Dispatch<SetStateAction<Album | null>>;
+  setModalVisible: Dispatch<SetStateAction<boolean>>;
+} | null>(null);
 
 const AlbumItem = ({ album }) => {
+  const { setModalData, setModalVisible } = useContext(ModalContext);
+
   return (
-    <View style={styles.item}>
-      <Image
-        source={{ uri: album.photos.data[0].thumbnailUrl }}
-        style={styles.photo}
-      />
-      <Text style={styles.title}>{album.title}</Text>
-    </View>
+    <Pressable
+      onPress={() => {
+        setModalData(album);
+        setModalVisible(true);
+      }}
+    >
+      <View style={styles.item}>
+        <Image
+          source={{ uri: album.photos.data[0].thumbnailUrl }}
+          style={styles.photo}
+        />
+        <Text style={styles.title}>{album.title}</Text>
+      </View>
+    </Pressable>
   );
 };
 
@@ -96,7 +118,6 @@ const AlbumsComponent = () => {
       },
     });
   };
-  console.log("🚀 ~ file: App.tsx:115 ~ AlbumsComponent ~ hasMore:", hasMore);
 
   return (
     <FlatList
@@ -124,11 +145,46 @@ const AlbumsComponent = () => {
 };
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalData, setModalData] = useState<Album | null>(null);
+
   return (
     <ApolloProvider client={client}>
-      <SafeAreaView style={styles.container}>
-        <AlbumsComponent />
-      </SafeAreaView>
+      <ModalContext.Provider value={{ setModalData, setModalVisible }}>
+        <SafeAreaView
+          style={[
+            styles.container,
+            modalVisible && styles.containerTransparent
+          ]}
+        >
+          <AlbumsComponent />
+          <Modal
+            animationType="fade"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Image
+                  source={{ uri: modalData?.photos.data[0].thumbnailUrl }}
+                  style={styles.modalPhoto}
+                />
+                <Text style={styles.modalText}>{modalData?.title}</Text>
+                <Pressable
+                  style={styles.button}
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text style={styles.textStyle}>Lukk</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+        </SafeAreaView>
+      </ModalContext.Provider>
     </ApolloProvider>
   );
 }
@@ -145,6 +201,9 @@ const styles = StyleSheet.create({
     width: "100%",
     flexShrink: 0,
   },
+  containerTransparent: {
+    backgroundColor: "rgba(0,0,0,0.5)",
+    },
   list: {
     width: "100%",
     flexGrow: 0,
@@ -164,5 +223,54 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     width: 60,
     height: 60,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 16,
+  },
+  modalView: {
+    width: "100%",
+    backgroundColor: "white",
+    borderRadius: 16,
+    padding: 16,
+    flexDirection: "column",
+    gap: 24,
+    flexShrink: 0,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    marginTop: 190,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    backgroundColor: "#180435",
+  },
+  textStyle: {
+    color: "white",
+    fontSize: 15,
+    fontWeight: "600",
+    textAlign: "center",
+  },
+  modalPhoto: {
+    borderRadius: 16,
+    width: 200,
+    height: 200,
+  },
+  modalText: {
+    marginBottom: 15,
+    fontSize: 36,
+    fontWeight: "500",
+    textTransform: "capitalize",
+    textAlign: "center",
   },
 });
